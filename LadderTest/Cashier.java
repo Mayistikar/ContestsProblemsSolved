85db8a37e2a2e3ebc9ab76ddff7a589306ebf4e9
andersonrodriguezce@gmail.com <andersonrodriguezce@gmail.com>
2016-10-06 23:55:19 -0500

    Cashier actualizado, con un uso de arreglos poco comun muy interesante

--- LadderTest/Cashier.java
+++ LadderTest/Cashier.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
-import java.util.LinkedList;
+import java.util.Arrays;
 
 class Cashier{
-	public static void addList(LinkedList<Node> listx, int numx){
-		for(int i=0; i<listx.size(); i++){
-			if(listx.get(i).getNum==numx){
-				listx.get(i).setCant(listx.getCant()+1);
-			}else if(numx>listx.get(i).getNum){
-				
-			}
-		}
+
+    public static void main(String[]args){
+	Scanner sc = new Scanner(System.in);
+
+	int[] list = new int[101];
+	int n = 101;
+	for(int i=0; i<list.length; i++){ // En jse 8 no es necesario inicializar el arreglo por que al crearlo el array se llena de ceros por defecto
+	    list[i]=0;
 	}
-	public static void main(String[]args){
-		Scanner sc = new Scanner(System.in);
-		int n = 101;
-		LinkedList<Node> list = new LinkedList<Node>();
-		while(n!=0){
-			
-			n=sc.nextInt();
-		}
+
+	while(n!=0){
+	    n=sc.nextInt();
+	    if(n!=0){
+		list[n]=list[n]+1; // En JSE 8 esta expresion se puede remplazar por list[n]++ osea que las posiciones de un arreglo entero son autoincrementables
+	    }
 	}
+	for(int x=0; x<list.length; x++){
+	    if(list[x]!=0){
+		System.out.println(x+" "+list[x]);
+	    }
+	}
+    }
 }
